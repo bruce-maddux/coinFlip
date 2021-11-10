eab55f3a915d60707f65d35c073b34ee5225cd96
unknown <brucema7@gmail.com>
2021-11-10 12:12:01 -0500

    First commit with base code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { render } from 'react-dom';
+import tailCoin from './tailCoin.jpg'
+import headCoin from './headcoin.jpg'
+import downCoin from './downcoin.jfif'
+import sideCoin from './sidecoin.jfif'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {random: 0, val : "D"};
+    this.handleClick = this.handleClick.bind(this);
+    this.urlLink = "";
+  }
+  handleClick() {
+    const min = 1;
+    const max = 100;
+    const rand = min + Math.random() * (max - min);
+    this.setState({val : rand > 40 && rand < 60 ? "M" : rand > 50 ? "H" : "T"});
+    this.setState({ random: rand });
+  }
+  render() {
+    {this.urlLink = this.state.val === "D" ? downCoin : this.state.val === "M" ? sideCoin : this.state.val === "H" ? headCoin : tailCoin}
+    return (
+      <div className="App">
+        <h1 className = "App-title">
+          Coin Flip
+        </h1>
+        <img src = {this.urlLink} className = "App-img"></img>
+        <button onClick={this.handleClick.bind(this)}>Click</button>
+        <div>The number is: {this.state.random}</div>
+        <div>Heads or Tails : {this.state.val}</div>
+
+      </div>
   );
 }
-
+}
 export default App;
+render(<App />, document.getElementById('root'));
+
